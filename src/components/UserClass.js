5f65d8e3f9ffa3cbf76ad06e5da25c526b9d5a29
HarshalJamdar <jamdarharshal@gmail.com>
2025-08-14 16:11:57 +0530

    fetch about us details api integreated

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,32 +5,33 @@ class UserClass extends React.Component {
         super(props);
 
         this.state = {
-            count: 0,
-            count2: 1,
+            userInfo: {
+                name: 'dummy',
+                location: 'default',
+                avatar_url: ''
+            }
         }
 
         console.log('constructor');
     };
 
-    componentDidMount(){
-        console.log('componentDidMount');
+    async componentDidMount(){
+        const data = await fetch('https://api.github.com/users/akshaymarch7');
+        const json = await data.json();
+
+        this.setState({
+            userInfo: json,
+        });
     }
 
     render() {
-        const { name, location} = this.props;
-        const { count, count2 } = this.state;
+       // const { name, location} = this.props;
 
-        console.log('render');
+        const { name, location, avatar_url } = this.state.userInfo
 
         return (
         <div className='user-card'>
-           <h1>Count:{count}</h1>
-           <button onClick={() =>{
-            this.setState({
-                count: this.state.count + 1,
-                count2: this.state.count2 + 1,
-            })
-           }}>Count Increase</button>
+            <img src={avatar_url} alt='img' />
            <h2>Name: {name}</h2> 
            <h3>Location: {location}</h3>
            <h4>Contact: jamdarharshal@gmail.com</h4>
